4c9ccef24931d3e2efc1724812d36d392ce8edf5
Van Huy <1712056@student.hcmus.edu.vn>
2021-04-23 11:19:02 +0700

    Design side menu

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,21 +1,46 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Avatar from "@material-ui/core/Avatar";
+import Collapse from "@material-ui/core/Collapse";
+
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
+import HomeIcon from "@material-ui/icons/Home";
+
+
 import Logo from "./../icons/ethereum.png";
 import LogoText from "./../images/Logo.png";
+import imgDashboard from "./../images/dashboard.svg";
+import imgSend from './../images/send.svg';
+
+const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    
+    display: "flex",
   },
-  appbar: {
+  appBar: {
     backgroundColor: "white",
-    paddingRight: "15%",
-    paddingLeft: "15%" 
-
+    padding: "0% 10%",
+    zIndex: theme.zIndex.drawer + 1,
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -24,28 +49,172 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   logoText: {
-    width: "25%",
+    width: "20%",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  drawerContainer: {
+    overflow: "auto",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
   },
 }));
 
-export default function AppBarMyCoin() {
+export default function AppBarMyCoin(props) {
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const isMenuOpen = Boolean(anchorEl);
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const menuId = "primary-search-account-menu";
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Setting</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Log out</MenuItem>
+    </Menu>
+  );
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.appbar}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <div style={{display: 'flex', alignItems: "center", }}>
-            <img className={classes.logo} src={Logo} alt="" style={{marginRight: '10px'}} />
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img
+              className={classes.logo}
+              src={Logo}
+              alt=""
+              style={{ marginRight: "10px" }}
+            />
             <img className={classes.logoText} src={LogoText} alt="" />
           </div>
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
-          <Button>New wallet</Button>
-          <Button>Access</Button>
+          <IconButton aria-label="show 11 new notifications">
+            <Badge badgeContent={11} color="secondary">
+              <NotificationsIcon fontSize="large" />
+            </Badge>
+          </IconButton>
+          <IconButton
+            edge="end"
+            aria-label="account of current user"
+            aria-controls={menuId}
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+          >
+            <AccountCircle fontSize="large" />
+          </IconButton>
+          <Button
+            variant="contained"
+            style={{
+              background: "white",
+              color: "blue",
+              border: "1px solid blue",
+              marginRight: "10px",
+            }}
+          >
+            New wallet
+          </Button>
+          <Button variant="contained" color="primary">
+            Access
+          </Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <Avatar src={imgDashboard} />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+            <ListItem button onClick={handleClick}>
+              <ListItemIcon>
+                <Avatar src={imgSend} />
+              </ListItemIcon>
+              <ListItemText primary="Send" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem button className={classes.nested}>
+                  <ListItemText primary="Send Transaction" />
+                </ListItem>
+              </List>
+              <List component="div" disablePadding>
+                <ListItem button className={classes.nested}>
+                  <ListItemText primary="Send Offline" />
+                </ListItem>
+              </List>
+            </Collapse>
+          </List>
+          <Divider />
+          <List>
+          <ListItem button>
+              <ListItemIcon>
+              <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+              <AssignmentIcon />
+              </ListItemIcon>
+              <ListItemText primary="New wallet" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+              <AssignmentIndIcon />
+              </ListItemIcon>
+              <ListItemText primary="Access" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {props.children}
+      </main>
+      {renderMenu}
     </div>
   );
 }
